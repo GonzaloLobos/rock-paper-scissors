8157c5f6e025d7cc2805ad98f06479c2505738b2
Gonzalo Lobos <loboscristiang@gmail.com>
2024-03-02 15:59:09 -0300

    Add input validation and redefine variables
    Input validation prevents possible errors.
    Variables were redefine to simplify code and
    enhance readability.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const choices = ["Rock", "Paper", "Scissors"];
+const choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice(){
   const randIdx = Math.floor(Math.random() * 3);
@@ -6,18 +6,19 @@ function getComputerChoice(){
 }
 
 function playRound(computerSelection, playerSelection){
-  if(computerSelection === playerSelection){
+  const lowerCasedSelection = playerSelection.toLowerCase();
+  if(computerSelection === lowerCasedSelection){
     console.log("It's a tie!");
     return 0; 
   } else if(
-    (computerSelection === "Rock" && playerSelection === "Scissors") ||
-    (computerSelection === "Paper" && playerSelection === "Rock") ||
-    (computerSelection === "Scissors" && playerSelection === "Paper")
+    (computerSelection === "Rock" && lowerCasedSelection === "Scissors") ||
+    (computerSelection === "Paper" && lowerCasedSelection === "Rock") ||
+    (computerSelection === "Scissors" && lowerCasedSelection === "Paper")
   ){
-    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    console.log(`You lose! ${toCapitalise(computerSelection)} beats ${toCapitalise(playerSelection)}`);
     return -1;
   } else {
-    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    console.log(`You win! ${toCapitalise(playerSelection)} beats ${toCapitalise(computerSelection)}`);
     return 1;
   }
 }
@@ -26,15 +27,29 @@ function playGame(){
   let result = 0;
   for(let i = 0; i < 5; i++){
     const userInput = prompt("Type in your choice (Rock, Paper or Scissors)", "Rock");
-    const parsedInput = userInput[0].toUpperCase() + userInput.slice(1).toLowerCase();
-    
-    result += playRound(getComputerChoice(), parsedInput)
+    const validData = validateInput(userInput);
+
+    result += playRound(getComputerChoice(), validData)
   }
   if(result > 0){
-    console.log("You won the game!")
+    alert("You won the game!")
   } else if(result < 0){
-    console.log("You lost the game :(")
+    alert("You lost the game :(")
   } else {
-    console.log("The game resulted in a tie")
+    alert("The game resulted in a tie")
   }
+}
+
+function validateInput(toValidate){
+  while(toValidate.toLowerCase() != "rock" && 
+        toValidate.toLowerCase() != "paper" && 
+        toValidate.toLowerCase() != "scissors"){
+    toValidate = prompt("Invalid data, please try again", "Rock");
+  }
+  return toValidate;
+}
+
+function toCapitalise(str){
+  const capitalisedStr = str[0].toUpperCase() + str.slice(1).toLowerCase()
+  return capitalisedStr;
 }
