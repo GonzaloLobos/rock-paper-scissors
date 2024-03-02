139f98aad318e07747caa7048249085c3b43c5b8
Gonzalo Lobos <loboscristiang@gmail.com>
2024-03-02 12:25:28 -0300

    Add random selection logic

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log("I am working!");
+// console.log("I am working!");
+
+const choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice(){
+  const randIdx = Math.floor(Math.random() * 3);
+  return choices[randIdx]
+}
