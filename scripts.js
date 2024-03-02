2642abd52625b9e1f9505ca33c612569727758d8
Gonzalo Lobos <loboscristiang@gmail.com>
2024-03-02 14:16:57 -0300

    Add round winner logic

--- scripts.js
+++ scripts.js
@@ -1,8 +1,23 @@
-// console.log("I am working!");
-
-const choices = ["rock", "paper", "scissors"];
+const choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice(){
   const randIdx = Math.floor(Math.random() * 3);
   return choices[randIdx]
+}
+
+function playRound(computerSelection, playerSelection){
+  if(computerSelection === playerSelection){
+    console.log("It's a tie!");
+    return 0; 
+  } else if(
+    (computerSelection === "Rock" && playerSelection === "Scissors") ||
+    (computerSelection === "Paper" && playerSelection === "Rock") ||
+    (computerSelection === "Scissors" && playerSelection === "Paper")
+  ){
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+    return -1;
+  } else {
+    console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+    return 1;
+  }
 }
