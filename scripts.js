d0a773b83362371911362bf9ede48b3f0809b2ee
Gonzalo Lobos <loboscristiang@gmail.com>
2024-03-11 12:51:29 -0300

    Add scores and winning logic

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 const buttons = document.querySelectorAll('button');
 const roundResult = document.querySelector('.round-result');
+const displayedPlayerScore = document.querySelector('.player-score');
+const displayedComputerScore = document.querySelector('.computer-score');
+const currentScores = document.querySelector('.current-scores')
 
 const choices = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
   const randIdx = Math.floor(Math.random() * 3);
@@ -11,18 +16,20 @@ function getComputerChoice(){
 function playRound(computerSelection, playerSelection){
   if(computerSelection === playerSelection){
     roundResult.textContent = "It's a tie!";
-    return 0; 
   } else if(
     (computerSelection === "rock" && playerSelection === "scissors") ||
     (computerSelection === "paper" && playerSelection === "rock") ||
     (computerSelection === "scissors" && playerSelection === "paper")
   ){
     roundResult.textContent = `You lose! ${toCapitalise(computerSelection)} beats ${toCapitalise(playerSelection)}`
-    return -1;
+    computerScore++;
   } else {
     roundResult.textContent = `You win! ${toCapitalise(playerSelection)} beats ${toCapitalise(computerSelection)}`
-    return 1;
+    playerScore++;
   }
+  displayedPlayerScore.textContent = playerScore;
+  displayedComputerScore.textContent = computerScore;
+  checkGameOver();
 }
 
 buttons.forEach((btn) => btn.addEventListener('click', function() {
@@ -30,24 +37,25 @@ buttons.forEach((btn) => btn.addEventListener('click', function() {
   playRound(getComputerChoice(),parsedSelection)
 }));
 
-// function playGame(){
-//   let result = 0;
-//   for(let i = 0; i < 5; i++){
-//     const userInput = prompt("Type in your choice (Rock, Paper or Scissors)", "Rock");
-//     const validData = validateInput(userInput);
-
-//     result += playRound(getComputerChoice(), validData)
-//   }
-//   if(result > 0){
-//     alert("You won the game!")
-//   } else if(result < 0){
-//     alert("You lost the game :(")
-//   } else {
-//     alert("The game resulted in a tie")
-//   }
-// }
-
 function toCapitalise(str){
   const capitalisedStr = str[0].toUpperCase() + str.slice(1).toLowerCase()
   return capitalisedStr;
+}
+
+function checkGameOver(){
+  if(playerScore === 5){
+    disableButtons();
+    const finalResult = document.createElement('p');
+    finalResult.textContent = "You won!";
+    currentScores.appendChild(finalResult);
+  } else if (computerScore === 5){
+    disableButtons();
+    const finalResult = document.createElement('p');
+    finalResult.textContent = "You lose!";
+    currentScores.appendChild(finalResult);
+  }
+}
+
+function disableButtons(){
+  buttons.forEach((btn) => btn.setAttribute("disabled", ""));
 }
